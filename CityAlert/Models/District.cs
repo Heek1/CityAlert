2834f74e2975166057ec8b211bb6ef848e8da811
Alina <AlinaBlednova2007@gmail.com>
2026-03-31 20:43:25 +0300

    Views Main page, Event page, Create Event, Subscriptions, Layout

--- CityAlert/Models/District.cs
+++ CityAlert/Models/District.cs
@@ -1,14 +1,16 @@
-﻿namespace CityAlert.Models
+﻿using System.Text.Json.Serialization;
+
+namespace CityAlert.Models
 {
     public class District
     {
         public int Id { get; set; }
-
         public string Name { get; set; } = string.Empty;
 
+        [JsonIgnore]
         public ICollection<Event> Events { get; set; } = new List<Event>();
 
+        [JsonIgnore]
         public ICollection<Subscription> Subscriptions { get; set; } = new List<Subscription>();
-
     }
 }
